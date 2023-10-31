a19968eff9db3f1e4d73649ea386f5463fbadd5a
sadok <sadok1991@gmail.com>
2023-10-31 18:07:01 +0100

    mise en place du css

--- index.php
+++ index.php
@@ -5,14 +5,23 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <script defer src="index.js"></script>
   <link rel="stylesheet" href="style.css">
+  <link href="https://fonts.googleapis.com/css2?family=Rubik:wght@300;400;700&display=swap" rel="stylesheet">
   <title>Todo</title>
 </head>
 <body>
-<header>
-  Header
-</header>
-<div class="content">content</div>
-<footer>footer</footer>
-
+<div class="container">
+  <header>
+  
+    <div class="logo">Pingucoder Tode</div>
+  </header>
+  <div class="content">
+    <div class="todo-container">
+      <h1>Ma Todo</h1>
+      <div class="todo-form"></div>
+      <div class="todo-list"></div>
+    </div>
+  </div>
+  <footer>2023  pingucoder	&copy;</footer>
+</div>
 </body>
 </html>
